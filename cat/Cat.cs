0062eee36adffd2e6a66d589968c82bf54315936
Elvina555 <152243987+Elvina555@users.noreply.github.com>
2024-11-01 20:58:25 +0500

    Add files via upload

---
+++ cat/Cat.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cat
+{
+    class Cat
+    {
+        private string name;
+        private double ves;
+        public string Name
+        {
+            // получение значения - просто возврат name
+            get
+            {
+                return name;
+            }
+            // установка значения - используем проверку
+            set
+            {
+                bool OnlyLetters = true;
+                // ключ. слово value - это то, что хотят свойству присвоить
+                foreach (var ch in value)
+                {
+                    if (!char.IsLetter(ch))
+                    {
+                        OnlyLetters = false;
+                    }
+                }
+
+                if (OnlyLetters)
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - неправильное имя!!!");
+                }
+            }
+        }
+        public double Ves
+        {
+            // получение значения - просто возврат name
+            get
+            {
+                return ves;
+            }
+            // установка значения - используем проверку
+            set
+            {
+                if (value > 0)
+                {
+                    ves = value;
+                }
+                else
+                {
+                    Console.WriteLine($"Вес {value} - неправельный вес!!!");
+                }
+            }
+        }
+        //конструктор
+        public Cat(string CatName,double CatVes)
+        {
+            Name = CatName;
+            Ves = CatVes;
+        }
+        //метод для опознания отдельно взятого кота
+        public void Meow()
+        {
+            Console.WriteLine($"{name}: МЯЯЯЯУ!!!!");
+            Console.WriteLine($"Вес:{ves} кг");
+        }
+        
+    }
+}
---
+++ cat/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Результат
+            try
+            {
+                Console.WriteLine("Введите число котов");
+                int count = int.Parse(Console.ReadLine());
+                for(int i=0;i<count;i++)
+                {
+                    Console.WriteLine($"Введите имя {i + 1} кота");
+                    string cats = Console.ReadLine();
+                    Console.WriteLine($"Введите вес {i + 1} кота");
+                    double ves = Convert.ToDouble(Console.ReadLine());
+                    Cat cat = new Cat(cats, ves);
+                    cat.Meow();
+                    Console.ReadKey();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("введите корректно");
+                Console.ReadKey();
+            }
+        }
+    }
+}
